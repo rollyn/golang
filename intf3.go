94db42cf2024da77477971e5a57ca776b958427e
unknown <rollyn.moises@gmail.com>
2018-11-27 03:45:25 -0800

    interface example

---
+++ intf3.go
@@ -0,0 +1,20 @@
+package main 
+
+
+
+import("fmt")
+
+type Person struct {
+	name string
+}
+
+func (p *Person) Speak() string {
+	return p.name + " Speak..."
+}
+
+func main() {
+	
+	p := Person{"acid"}
+
+	fmt.Println( p.Speak() )
+}
