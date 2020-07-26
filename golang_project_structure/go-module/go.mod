3d0f60bcd2cf74cfe2ef2df1bc3b75c8a2c368ec
Ethanium <712031+rollyn@users.noreply.github.com>
2020-07-26 18:07:37 +0800

    adding go modules

---
+++ golang_project_structure/go-module/go.mod
@@ -0,0 +1,3 @@
+module ethanium.com/go-module
+
+go 1.14
---
+++ golang_project_structure/go-module/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+  "fmt"
+  "ethanium.com/go-module/libraries"
+)
+
+func main() {
+  fmt.Printf("hello"+libraries.World())
+}
