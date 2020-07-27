655938a08e64e0eadcfbb7dbb103c38bad8afb9c
Ethanium <712031+rollyn@users.noreply.github.com>
2020-07-27 21:49:13 +0800

    Microservice Example - Go, Gin, MongoDB

---
+++ task-management/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"log"
+	"net/http" 
+        "task-management/db"
+        t "task-management/task"
+	"github.com/gin-gonic/gin"
+)
+
+func handleGetTasks(c *gin.Context) {
+	var loadedTasks, err = db.GetAllTasks()
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"msg": err})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"tasks": loadedTasks})
+}
+
+func handleGetTask(c *gin.Context) {
+	var task t.Task
+	if err := c.BindUri(&task); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"msg": err})
+		return
+	}
+	var loadedTask, err = db.GetTaskByID(task.ID)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"msg": err})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"ID": loadedTask.ID, "Body": loadedTask.Body})
+}
+
+func handleCreateTask(c *gin.Context) {
+	var task t.Task
+	if err := c.ShouldBindJSON(&task); err != nil {
+		log.Print(err)
+		c.JSON(http.StatusBadRequest, gin.H{"msg": err})
+		return
+	}
+	id, err := db.Create(&task)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"msg": err})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"id": id})
+}
+
+func handleUpdateTask(c *gin.Context) {
+	var task t.Task
+	if err := c.ShouldBindJSON(&task); err != nil {
+		log.Print(err)
+		c.JSON(http.StatusBadRequest, gin.H{"msg": err})
+		return
+	}
+	savedTask, err := db.Update(&task)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"msg": err})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"task": savedTask})
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/tasks/:id", handleGetTask)
+	r.GET("/tasks/", handleGetTasks)
+	r.PUT("/tasks/", handleCreateTask)
+	r.POST("/tasks/", handleUpdateTask)
+	r.Run(":5000") // listen and serve on 0.0.0.0:8080
+}
