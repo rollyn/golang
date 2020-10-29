50ce05892108e2125b0680cb4d6e0c03cdbb8007
Ethanium <712031+rollyn@users.noreply.github.com>
2020-10-29 15:32:40 +0800

    Golang GRPC

--- grpc-go/greet/greet_client/client.go
+++ grpc-go/greet/greet_client/client.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"github.com/rollyn/grpc-go/greet/greetpb"
 	"google.golang.org/grpc"
+	"time"
 )
 
 func main() {
@@ -18,7 +20,137 @@ func main() {
 	defer conn.Close()
 	c := greetpb.NewGreetServiceClient(conn)
 	//fmt.Printf("created client %f",c)
-	doUnary(c)
+//	doUnary(c)
+
+	//doServerStreaming(c)
+	//doClientStreaming(c)
+	doBidirectionalStream(c)
+}
+
+func doBidirectionalStream(c greetpb.GreetServiceClient) {
+	fmt.Println("executing doBidirectionalStream...")
+
+	requests := []*greetpb.GreetEveryoneRequest{
+		&greetpb.GreetEveryoneRequest{
+			Greeting:&greetpb.Greeting{
+				FirstName:"ROLLYN",
+			},
+		},
+		&greetpb.GreetEveryoneRequest{
+			Greeting:&greetpb.Greeting{
+				FirstName:"JOY",
+			},
+		},
+		&greetpb.GreetEveryoneRequest{
+			Greeting:&greetpb.Greeting{
+				FirstName:"ETHAN",
+			},
+		},
+		&greetpb.GreetEveryoneRequest{
+			Greeting:&greetpb.Greeting{
+				FirstName:"CHENILLE",
+			},
+		},
+	}
+
+	stream, err := c.GreetEveryone(context.Background())
+	if err != nil {
+		log.Fatalf("unable to stream %v", err)
+		return
+	}
+	wc := make(chan struct{})
+
+	go func() {
+		// send
+		for _,req := range requests {
+			fmt.Printf("Sending request %v\n",req)
+			stream.Send(req)
+			time.Sleep(1000 * time.Millisecond)
+		}
+		stream.CloseSend()
+	}()
+	go func() {
+		// receive
+		for {
+			res, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalf("unable to receive %v", err)
+				break
+			}
+			fmt.Printf("Received: %v\n", res)
+		}
+		close(wc)
+	}()
+	<-wc
+}
+
+func doClientStreaming(c greetpb.GreetServiceClient) {
+	fmt.Println("executing doClientStreaming...")
+	requests := []*greetpb.LongGreetRequest{
+		&greetpb.LongGreetRequest{
+			Greet:&greetpb.Greeting{
+				FirstName:"ROLLYN",
+			},
+		},
+		&greetpb.LongGreetRequest{
+			Greet:&greetpb.Greeting{
+				FirstName:"JOY",
+			},
+		},
+		&greetpb.LongGreetRequest{
+			Greet:&greetpb.Greeting{
+				FirstName:"ETHAN",
+			},
+		},
+		&greetpb.LongGreetRequest{
+			Greet:&greetpb.Greeting{
+				FirstName:"CHENILLE",
+			},
+		},
+	}
+
+	stream, err := c.LongGreet(context.Background())
+	if err != nil {
+		log.Fatalf("unable to stream %v", err)
+	}
+	for _, req := range requests {
+		fmt.Printf("Sending request %v\n", req)
+		stream.Send(req)
+		time.Sleep(1000 * time.Millisecond)
+	}
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("error while receiving %v", err)
+	}
+	fmt.Printf("Response: %v\n", res)
+}
+func doServerStreaming(c greetpb.GreetServiceClient) {
+	fmt.Println("executing doServerStreaming...")
+	req := &greetpb.GreetManyTimesRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "ROLLYN",
+			LastName:"MOISES",
+		},
+	}
+	resStream, err := c.GreetManyTimes(context.Background(), req)
+	if err != nil {
+		log.Fatalf("unable to receive %v", err)
+	}
+	for {
+		msg, err := resStream.Recv()
+		if err == io.EOF {
+			// close stream
+			break
+		}
+		if err != nil {
+			log.Fatalf("error while reading stream %v", err)
+		}
+		fmt.Printf("Response: %v\n",msg.GetResult())
+	}
+
 }
 
 func doUnary(c greetpb.GreetServiceClient) {
--- grpc-go/greet/greet_server/server.go
+++ grpc-go/greet/greet_server/server.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 	"github.com/rollyn/grpc-go/greet/greetpb"
 	"google.golang.org/grpc"
+	"io"
 	"log"
 	"net"
+	"strconv"
+	"time"
 )
 
 type server struct{}
@@ -23,6 +26,65 @@ func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.G
 	return res, nil
 }
 
+func (*server) GreetManyTimes(req *greetpb.GreetManyTimesRequest, stream greetpb.GreetService_GreetManyTimesServer) error {
+
+	fmt.Printf("GreetManyTimes function invoked %v", req)
+
+	firstName := req.GetGreeting().GetLastName()
+	lastName := req.GetGreeting().GetLastName()
+	for i := 0; i<10;  i++ {
+		result := "Hello " + firstName + " " + lastName + ": " + strconv.Itoa(i)
+		res := &greetpb.GreetManyTimesResponse{
+			Result: result,
+		}
+		stream.Send(res)
+		time.Sleep(1000 * time.Millisecond)
+	}
+	return nil
+}
+
+func (*server) GreetEveryone(stream greetpb.GreetService_GreetEveryoneServer) error {
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			log.Fatalf("Failed to receive stream %v", err)
+			return err
+		}
+		firstName := req.GetGreeting().FirstName
+		result := "Hello " + firstName + "!"
+		sendError := stream.Send(&greetpb.GreetEveryoneResponse{
+			Result:result,
+		})
+		if sendError != nil {
+			log.Fatalf("Failed to send stream %v", sendError)
+			return sendError
+		}
+	}
+}
+
+func (*server) LongGreet(stream greetpb.GreetService_LongGreetServer) error {
+
+	result := ""
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&greetpb.LongGreetResponse{
+				Result:result,
+			})
+		}
+		if err != nil {
+			log.Fatalf("Failed to receive stream %v", err)
+		}
+		firstName := req.GetGreet().FirstName
+		result += "Hello "+ firstName + "\n"
+	}
+
+
+	return nil
+}
 
 func main() {
 
